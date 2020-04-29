8e44d14fd690ecede174aa020bb05c761d16ed5d
Liutx <15666882775@163.com>
2020-04-29 21:53:28 +0800

    页面新增,基础信息设置/职位管理批量删除功能完成

--- src/views/sys/basic/JobLevelMana.vue
+++ src/views/sys/basic/JobLevelMana.vue
@@ -49,9 +49,14 @@
                 <el-table-column
                         label="是否启用">
                     <template slot-scope="scope">
-                        <el-tag type="success" v-if="scope.row.enabled">已启用</el-tag>
+                        <el-tag v-if="scope.row.enabled" type="success">已启用</el-tag>
                         <el-tag type="danger" v-else>未启用</el-tag>
                     </template>
+                    <el-switch
+                            v-model="updatejl.enabled"
+                            active-color="#13ce66"
+                            inactive-color="#ff4949">
+                    </el-switch>
                 </el-table-column>
                 <el-table-column
                         label="操作">
@@ -62,6 +67,56 @@
                 </el-table-column>
             </el-table>
         </div>
+        <el-dialog
+            title="修改职称"
+            :visible.sync="dialogVisible"
+            width="30%">
+            <div>
+                <table>
+                    <tr>
+                        <td>
+                            <el-tag>职称名</el-tag>
+                        </td>
+                        <td>
+                            <el-input size="small" v-model="updatejl.name"></el-input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>职称级别</el-tag>
+                        </td>
+                        <td>
+                            <el-select v-model="updatejl.titleLevel" placeholder="职称等级" size="small" style="margin-left: 5px">
+                                <el-option
+                                        v-for="item in titleLevel"
+                                        :key="item"
+                                        :label="item"
+                                        :value="item">
+                                </el-option>
+                            </el-select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <el-tag>是否启用</el-tag>
+                        </td>
+                        <td>
+                            <el-switch
+                                    v-model="updatejl.enabled"
+                                    active-text="启用"
+                                    inactive-text="禁用"
+                                    active-color="#13ce66"
+                                    inactive-color="#ff4949">
+                            </el-switch>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <span slot="footer" class="dialog-footer">
+    <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+    <el-button size="small" type="primary" @click="doUpdate">确 定</el-button>
+  </span>
+        </el-dialog>
     </div>
 </template>
 
@@ -86,7 +141,14 @@ export default {
                     '中级',
                     '高级',
                     '总监'
-                ]
+                ],
+                dialogVisible:false,
+                updatejl:{
+                    name:'',
+                    titleLevel:'',
+                    enabled:false
+                },
+                multipleSelection: [],
             }
         },
     mounted() {
@@ -134,6 +196,20 @@ export default {
                         message : '已取消删除'
                     })
                 })
+            },
+            showEditView(data){
+                this.dialogVisible = true;
+                Object.assign(this.updatejl,data);
+            },
+            //更新，请求服务端
+            doUpdate(){
+                putRequest("/system/basic/joblevel/",this.updatejl).then(resp => {
+                    this.initJls()
+                    this.dialogVisible = false;
+                })
+            },
+            handleSelectionChange(val){
+                multipleSelection=val;
             }
         }
     }
