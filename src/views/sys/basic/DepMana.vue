c09a94cf88d5ec8886100441da4e5a77dda4bd4c
Liutx <15666882775@163.com>
2020-05-20 22:30:41 +0800

    部门管理init

--- src/views/sys/basic/DepMana.vue
+++ src/views/sys/basic/DepMana.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        部门管理
+        <el-input
+                placeholder="输入部门名称进行搜索..."
+                v-model="filterText">
+        </el-input>
+
+        <el-tree
+                class="filter-tree"
+                :data="deps"
+                :props="defaultProps"
+                default-expand-all
+                :filter-node-method="filterNode"
+                ref="tree">
+        </el-tree>
     </div>
 </template>
 
 <script>
+    //todo:16:31
     export default {
-        name: "DepMana"
+        name: "DepMana",
+        data() {
+            return{
+                filterText: '',
+                deps:[],
+                defaultProps: {
+                    children: 'children',
+                    label: 'name'
+                }
+            }
+        }
     }
 </script>
 
